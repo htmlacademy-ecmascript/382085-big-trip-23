8feab5f2312f5ce1b094550c2c9169b149f2d59d
George G <grreulvi@gmail.com>
2024-05-27 00:42:55 +0300

    [module1-task3] удалил лишний код из main

--- src/main.js
+++ src/main.js
@@ -2,54 +2,6 @@ import { RenderPosition, render } from './render.js';
 import TripEventsPresenter from './trip-events-presenter';
 import { TripInfoView } from './view/trip-info';
 
-/**
-* @typedef {Object} Offer
-* @prop {string} title
-* @prop {number} price
-*/
-
-/**
-* @typedef {Object} WaypointData
-* @prop {string} title
-* @prop {Date} from
-* @prop {Date} to
-* @prop {number} price
-* @prop {Offer[]} offers
-*/
-
-// mock data
-const WAYPOINT_DATA = [
-  {
-    title: 'waypoint one',
-    from: new Date('2024-06-01T12:00'),
-    to: new Date('2024-06-09T12:00'),
-    price: 150,
-    offers: [
-      { title: 'offer one', price: 150 },
-      { title: 'offer two', price: 150 }
-    ]
-  },{
-    title: 'waypoint two',
-    from: new Date('2024-06-01T12:00'),
-    to: new Date('2024-06-09T12:00'),
-    price: 150,
-    offers: [
-      { title: 'offer one', price: 150 },
-      { title: 'offer two', price: 150 }
-    ]
-  },
-  {
-    title: 'waypoint three',
-    from: new Date('2024-06-01T12:00'),
-    to: new Date('2024-06-09T12:00'),
-    price: 150,
-    offers: [
-      { title: 'offer one', price: 150 },
-      { title: 'offer two', price: 150 }
-    ]
-  }
-];
-
 const eventsContainer = document.querySelector('.trip-events');
 const tripMainContainer = document.querySelector('.trip-main');
 
@@ -58,4 +10,3 @@ render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
 const eventsPresenter = new TripEventsPresenter({eventsContainer});
 
 eventsPresenter.init();
-
