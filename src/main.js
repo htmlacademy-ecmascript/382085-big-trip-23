4681636b354e93839e5e158d3dece555547926f6
George G <grreulvi@gmail.com>
2024-05-26 23:49:36 +0300

    [module1-task3] переделал представления на классы, добавил тестовых данных

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-//import { editWaypoint } from 'view/edit-waypoint';
-import { filter } from './view/filter';
-import { sort } from './view/sort';
-import { waypoint } from './view/waypoint';
-
-const filterContainer = document.querySelector('.trip-controls__filters');
-const filterMarkup = filter();
-filterContainer.innerHTML = filterMarkup;
+import { render, RenderPosition } from './render';
+import WaypointView from './view/waypoint';
+import FilterView from './view/filter';
+import SortView from './view/sort';
+import EditWaypointView from './view/edit-waypoint';
 
 /**
 * @typedef {Object} Offer
@@ -21,21 +18,50 @@ filterContainer.innerHTML = filterMarkup;
 * @prop {number} price
 * @prop {Offer[]} offers
 */
-const WAYPOINT_DATA = {
-  title: 'waypoint one',
-  from: new Date('2024-06-01T12:00'),
-  to: new Date('2024-06-09T12:00'),
-  price: 150,
-  offers: [
-    { title: 'offer one', price: 150 },
-    { title: 'offer two', price: 150 }
-  ]
-};
 
+// mock data
+const WAYPOINT_DATA = [
+  {
+    title: 'waypoint one',
+    from: new Date('2024-06-01T12:00'),
+    to: new Date('2024-06-09T12:00'),
+    price: 150,
+    offers: [
+      { title: 'offer one', price: 150 },
+      { title: 'offer two', price: 150 }
+    ]
+  },{
+    title: 'waypoint two',
+    from: new Date('2024-06-01T12:00'),
+    to: new Date('2024-06-09T12:00'),
+    price: 150,
+    offers: [
+      { title: 'offer one', price: 150 },
+      { title: 'offer two', price: 150 }
+    ]
+  },
+  {
+    title: 'waypoint three',
+    from: new Date('2024-06-01T12:00'),
+    to: new Date('2024-06-09T12:00'),
+    price: 150,
+    offers: [
+      { title: 'offer one', price: 150 },
+      { title: 'offer two', price: 150 }
+    ]
+  }
+];
+
+const filterContainer = document.querySelector('.trip-controls__filters');
 const eventsContainer = document.querySelector('.trip-events');
-const sortMarkup = sort();
-const waypointMarkup = waypoint(WAYPOINT_DATA);
 
-eventsContainer.innerHTML = `${sortMarkup} ${waypointMarkup}`;
+const waypoints = Array.from(WAYPOINT_DATA, (item) => new WaypointView(item));
 
-//editWaypoint();
+for (const waypoint of waypoints) {
+  render(waypoint, eventsContainer);
+}
+render(new SortView(), eventsContainer, RenderPosition.BEFOREBEGIN);
+render(new EditWaypointView, eventsContainer, RenderPosition.AFTERBEGIN);
+
+render(new FilterView(), filterContainer);
+
