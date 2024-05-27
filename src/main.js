a619db36ea925b96597a18ac352473e8ad9cbfeb
George G <grreulvi@gmail.com>
2024-05-28 01:51:19 +0300

    [module2-task1] подключил модели, передал их в презентер

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import { RenderPosition, render } from './render.js';
 import TripEventsPresenter from './trip-events-presenter';
 import { TripInfoView } from './view/trip-info';
+import WaypointsModel from './model/waypoints-model.js';
+import DestinationsModel from './model/destinations-model.js';
+import OffersModel from './model/offers-model.js';
 
 const eventsContainer = document.querySelector('.trip-events');
 const tripMainContainer = document.querySelector('.trip-main');
 
+const waypointsModel = new WaypointsModel();
+const destinationsModel = new DestinationsModel();
+const offersModel = new OffersModel();
+
 render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
 
-const eventsPresenter = new TripEventsPresenter({eventsContainer});
+const eventsPresenter = new TripEventsPresenter({
+  eventsContainer,
+  waypointsModel,
+  destinationsModel,
+  offersModel
+});
 
 eventsPresenter.init();
