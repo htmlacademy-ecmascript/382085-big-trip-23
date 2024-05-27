409f3a7aab24d23b524dabb53334c26fa2262c6f
George G <grreulvi@gmail.com>
2024-05-28 01:53:58 +0300

    [module2-task1] добавил функцию выбора случайного элемента массива

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
 import dayjs from 'dayjs';
+
+export function getRandomArrayElement(anArray) {
+  return anArray[Math.floor(Math.random() * anArray.length)];
+}
+
 const DATE_FORMAT = 'D MMMM';
 
 export function humanizeTaskDueDate(dueDate) {
