075c49b1dcc90f4a0a0469830dfc9fa777a328ec
George G <grreulvi@gmail.com>
2024-05-29 02:21:35 +0300

    [module2-task1] почти закончил с шаблоном для пункта путешествия

--- src/view/waypoint.js
+++ src/view/waypoint.js
@@ -1,7 +1,11 @@
 import { createElement } from '../render';
+import { EVENT_TYPE_ICONS } from '../../constants';
+import dayjs from 'dayjs';
+import { getDurationString } from '../utils';
 
 /**
 * @param {import('../mock/offers').Offer} offer
+* @returns {string} вёрстка одной дополнительной опции
 */
 function getOffer(offer) {
   return `
@@ -14,43 +18,54 @@ function getOffer(offer) {
 
 
 /**
-* @param {import('../mock/trip').Waypoint} waypointData
+* @param {import('../mock/trip').Waypoint} waypoint
+* @param {import('../mock/destinations').Destination} destination объект Destination соответствующий назначению пункта путешествия
+* @param {import('../mock/offers').Offer} offers опции для текущего типа пункта путешествия
+* @returns {string} вёрстка для одного пункта в путешествии
 */
-function waypoint(waypointData) {
+function createWaypointTemplate(waypoint, destination, offers) {
 
   const offersMarkup = [];
-  for (const offer of waypointData.offers) {
-    offersMarkup.push(getOffer(offer));
+  // TODO уточнить тут все доступные, или все подключённые?
+  for (const offer of waypoint.offers) {
+    const offerObject = offers.find((item) => item.id === offer);
+    offersMarkup.push(getOffer(offerObject));
   }
 
-  const eventDuration = '40 M';
-  const eventDate = new Date(waypointData.date_from).toLocaleString('en-us', {month: 'short', date: '2-digit'});
-  const eventDateAttrib = new Date(waypointData.date_from).toISOString().slice(0, 10);
+  const eventDate = dayjs(waypoint.date_from).format('MMM DD'); //new Date(waypoint.date_from).toLocaleString('en-us', {month: 'short', date: '2-digit'});
+  const eventDateAttrib = waypoint.date_from; //new Date(waypoint.date_from).toISOString().slice(0, 10);
+  const eventIcon = EVENT_TYPE_ICONS[waypoint.type];
 
+  const dayjsFrom = dayjs(waypoint.date_from);
+  const dayjsTo = dayjs(waypoint.date_to);
+
+  const humanizedInterval = getDurationString(dayjsFrom, dayjsTo);
+
+  // TODO в цене писать полную стойиость с опциями? или только base_price?
   return `
 <li class="trip-events__item">
   <div class="event">
     <time class="event__date" datetime="${eventDateAttrib}">${eventDate}</time>
     <div class="event__type">
-      <img class="event__type-icon" width="42" height="42" src="img/icons/check-in.png" alt="Event type icon">
+      <img class="event__type-icon" width="42" height="42" src="${eventIcon}" alt="Event type icon">
     </div>
-    <h3 class="event__title">${waypointData.title}</h3>
+    <h3 class="event__title">${destination.name}</h3>
     <div class="event__schedule">
       <p class="event__time">
-        <time class="event__start-time" datetime="2019-03-18T12:25">16:20</time>
+        <time class="event__start-time" datetime="${waypoint.date_from}">${dayjsFrom.format('HH:MM')}</time>
         &mdash;
-        <time class="event__end-time" datetime="2019-03-18T13:35">17:00</time>
+        <time class="event__end-time" datetime="${waypoint.date_to}">${dayjsTo.format('HH:MM')}</time>
       </p>
-      <p class="event__duration">${eventDuration}</p>
+      <p class="event__duration">${humanizedInterval}</p>
     </div>
     <p class="event__price">
-      &euro;&nbsp;<span class="event__price-value">${waypointData.base_price}</span>
+      &euro;&nbsp;<span class="event__price-value">${waypoint.base_price}</span>
     </p>
     <h4 class="visually-hidden">Offers:</h4>
     <ul class="event__selected-offers">
       ${offersMarkup.join(' ')}
     </ul>
-    <button class="event__favorite-btn event__favorite-btn--active" type="button">
+    <button class="event__favorite-btn ${waypoint.is_favorite ? 'event__favorite-btn--active' : ''}" type="button">
       <span class="visually-hidden">Add to favorite</span>
       <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
         <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -65,13 +80,14 @@ function waypoint(waypointData) {
 
 export default class WaypointView {
 
-  // пока так
-  constructor(data) {
-    this.data = data;
+  constructor({waypoint, destination, offers}) {
+    this.waypoint = waypoint;
+    this.destination = destination;
+    this.offers = offers;
   }
 
   getTemplate() {
-    return waypoint(this.data);
+    return createWaypointTemplate(this.waypoint, this.destination, this.offers);
   }
 
   getElement() {
