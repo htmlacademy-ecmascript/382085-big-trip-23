958c4e91ba110205caff20904962fdd67244fd3f
George G <grreulvi@gmail.com>
2024-05-29 02:04:18 +0300

    [module2-task1] добавил файл с константами

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const EVENT_TYPE_ICONS = {
+  'check-in': 'img/icons/check-in.png',
+  'sightseeing': 'img/icons/sightseeing.png',
+  'restaurant': 'img/icons/restaurant.png',
+  'taxi': 'img/icons/taxi.png',
+  'bus': 'img/icons/bus.png',
+  'train': 'img/icons/train.png',
+  'ship': 'img/icons/ship.png',
+  'drive': 'img/icons/drive.png',
+  'flight': 'img/icons/flight.png'
+};
