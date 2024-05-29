fe93a52ff10398f726e54bbe6326dd69f10d5c3c
George G <grreulvi@gmail.com>
2024-05-30 02:53:45 +0300

    [module2-task1] поменял экспорт в tripInfoView на экспорт по умолчанию

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { RenderPosition, render } from './render.js';
 import TripEventsPresenter from './trip-events-presenter';
-import { TripInfoView } from './view/trip-info';
+import TripInfoView from './view/trip-info';
 import WaypointsModel from './model/waypoints-model.js';
 import DestinationsModel from './model/destinations-model.js';
 import OffersModel from './model/offers-model.js';
