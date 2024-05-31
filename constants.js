5bcbc03b6bee81affaae23d147532f3fc937ef0b
George G <grreulvi@gmail.com>
2024-05-31 04:49:58 +0300

    [module2-task1] добавил константы для сотрировки и фильтров

--- constants.js
+++ constants.js
@@ -9,3 +9,29 @@ export const EVENT_TYPE_ICONS = {
   'drive': 'img/icons/drive.png',
   'flight': 'img/icons/flight.png'
 };
+
+
+/** @type {import('./mock/trip').Waypoint} */
+export const DUMMY_WAYPOINT = {
+  id: 'none',
+  type: 'train',
+  dateTo: (new Date()).toISOString(),
+  dateFrom: (new Date()).toISOString(),
+  basePrice: 0,
+  offers: [],
+  destination: 'kislovodsk-park',
+  isFavorite: false,
+};
+
+export const FILTER_ITEMS = ['Everything', 'Future', 'Present', 'Past'];
+export const DEFAULT_FILTER_ITEM_INDEX = 0;
+
+export const SORT_ITEMS = [
+  {canSort: true, title: 'Day'},
+  {canSort: false, title: 'Event'},
+  {canSort: true, title: 'Time'},
+  {canSort: true, title: 'Price'},
+  {canSort: false, title: 'Offer'}
+];
+
+export const DEFAULT_SORT_ITEM_INDEX = 0;
