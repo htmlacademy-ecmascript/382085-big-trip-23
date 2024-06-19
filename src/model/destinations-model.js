2891d930c1d7aa10188fb1d45ffa2c9206430287
vireulgr <grreulvi@gmail.com>
2024-06-19 06:32:49 +0300

    [final] Добавил в модели возможность подписаться и событие ошибки начальной загрузки

--- src/model/destinations-model.js
+++ src/model/destinations-model.js
@@ -1,10 +1,14 @@
-export default class DestinationsModel {
+import { UpdateType } from '../constants';
+import Observable from '../framework/observable';
+
+export default class DestinationsModel extends Observable {
   /** @type {import("../constants").Destination[]} */
   #destinations = [];
 
   #apiService = null;
 
   constructor({apiService}) {
+    super();
     this.#apiService = apiService;
   }
 
@@ -20,8 +24,16 @@ export default class DestinationsModel {
   async init() {
     try {
       this.#destinations = await this.#apiService.destinations;
+      // debug
+//      this.#destinations = [ { "id": "no-pictures-destination", "description": "No pictures destination description", "name": "No pictures destination", "pictures": [] },
+//  { "id": "full-destination", "description": "Full destination description", "name": "Full destination", "pictures": [ { "src": "https://21.objects.pages.academy/static/destinations/4.jpg", "description": "First picture" }, { "src": "https://21.objects.pages.academy/static/destinations/15.jpg", "description": "Second picture" }, { "src": "https://21.objects.pages.academy/static/destinations/4.jpg", "description": "Third picture" } ] },
+//  { "id": "empty-destination", "name": "Empty destination", "description": "", "pictures": [] } ];
     } catch (err) {
       this.#destinations = [];
+      this._notify(UpdateType.INIT_FAILED, this.#destinations);
+      console.log('destinations model init failed');
+      return;
     }
+    this._notify(UpdateType.INIT, this.#destinations);
   }
 }
